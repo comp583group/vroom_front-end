4991fe28d6244ff2d1d84bfa543d151bf02f8ddd
thomaschor <99774559+thomaschor@users.noreply.github.com>
2025-03-12 14:51:52 -0700

    Add image for New Inventory square on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -187,7 +187,7 @@ export default function Home() {
             <div className="bg-gray-100 rounded-lg overflow-hidden shadow-md transition-transform hover:scale-105">
               <div className="h-48 relative">
                 <Image
-                  src="/api/placeholder/800/600"
+                  src="/images/samuele-errico-piccarini-MyjVReZ5GLQ-unsplash.jpg"
                   alt="New Cars"
                   fill
                   style={{ objectFit: "cover" }}
